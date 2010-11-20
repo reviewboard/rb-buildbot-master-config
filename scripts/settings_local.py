0d36374343f07e3ffe174636d74018d89fc011ae
Christian Hammond <chipx86@chipx86.com>
2010-11-19 19:08:40 -0800

    Update settings_local.py to use the new Django 1.2 database config settings.

--- scripts/settings_local.py
+++ scripts/settings_local.py
@@ -1,5 +1,9 @@
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = 'reviewboard.db'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'reviewboard.db',
+    }
+}
 
 SECRET_KEY = "hjdsfkjaysf98y32iuhayfw87ea6r23hjk"
 
